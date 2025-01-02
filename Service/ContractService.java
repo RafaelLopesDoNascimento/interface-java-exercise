a822e7679558dbc426ccce900af57dbe739bff64
Rafael Lopes <rafaellopesdonascimento2607@gmail.com>
2025-01-02 13:24:23 -0300

    Update in the Contract about the Arraylist of Installments

--- Service/ContractService.java
+++ Service/ContractService.java
@@ -1,8 +1,10 @@
 package Service;
 
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import Domain.entities.Contract;
+import Domain.entities.Installment;
 
 public class ContractService {
 	public ContractService() {
@@ -14,15 +16,19 @@ public class ContractService {
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		PaypalService payPalService = new PaypalService();
 		System.out.println("Parcelas:");
+		LocalDate dateSupport = contract.getDate();
 
 		for (int i = 1; i <= months; i++) {
-			contract.setDate(contract.getDate().plusMonths(1));
+			dateSupport = dateSupport.plusMonths(1);
+//			contract.setDate(contract.getDate().plusMonths(1));
 			Double interestParam = payPalService.interest(contract.getTotalValue(), i);
 			Double toAddpaymentFeeParam = payPalService.paymentFee(contract.getTotalValue() / months + interestParam);
 			Double paymentFeeParam = contract.getTotalValue() / months + interestParam;
 
-			System.out.print(contract.getDate().format(dtf));
+			System.out.print(dateSupport.format(dtf));
 			System.out.printf(" - %.2f%n", toAddpaymentFeeParam + paymentFeeParam);
+			Installment installmentParam = new Installment(contract.getDate().plusMonths(1), (toAddpaymentFeeParam + paymentFeeParam));
+			contract.addInstallment(installmentParam);
 		}
 	}
 }
